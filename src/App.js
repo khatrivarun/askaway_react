a2a04abfc53c4639c18e3cedc757cbd802311f96
Varun Khatri <43516016+khatrivarun@users.noreply.github.com>
2021-01-20 21:43:47 +0530

    PrivateRoute Component Setup. Closes #2

--- src/App.js
+++ src/App.js
@@ -1,34 +1,10 @@
-import firebase from 'firebase/app';
-import firebaseConfig from './config/firebase';
-import ReduxThunk from 'redux-thunk';
-import { useEffect } from 'react';
-import { Provider as ReduxProvider } from 'react-redux';
-import {
-  applyMiddleware,
-  combineReducers,
-  createStore,
-} from '@reduxjs/toolkit';
-import authReducer from './store/reducers/auth';
+import Routes from './components/routes/Routes';
 
 const App = () => {
-  useEffect(() => {
-    if (!firebase.apps.length) {
-      firebase.initializeApp(firebaseConfig);
-    }
-  }, []);
-
-  const reducer = combineReducers({
-    auth: authReducer,
-  });
-
-  const reduxStore = createStore(reducer, applyMiddleware(ReduxThunk));
-
   return (
-    <ReduxProvider store={reduxStore}>
-      <div>
-        <h1>Oii</h1>
-      </div>
-    </ReduxProvider>
+    <div>
+      <Routes />
+    </div>
   );
 };
 
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,2 +1,14 @@
 export const SIGN_IN = 'SIGN IN';
 export const SIGN_OUT = 'SIGN OUT';
+
+export const autoLogin = (uid) => {
+  return async (dispatch) => {
+    // TODO: GET USER DATA FROM FIRESTORE
+    dispatch({
+      type: SIGN_IN,
+      payload: {
+        uid: uid,
+      },
+    });
+  };
+};
