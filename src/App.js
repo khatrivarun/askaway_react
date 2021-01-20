4b06ff289a70c797da6b35a2b82fb2266d381fbd
Varun Khatri <43516016+khatrivarun@users.noreply.github.com>
2021-01-20 19:27:50 +0530

    Firebase Setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import firebase from 'firebase/app';
+import firebaseConfig from './config/firebase';
+
 const App = () => {
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  }
+
   return (
-    <div className='App'>
+    <div>
       <h1>Oii</h1>
     </div>
   );
